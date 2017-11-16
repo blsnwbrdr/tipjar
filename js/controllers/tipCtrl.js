6bca607f1df9fb787f9b2c303c75d331d7000b69
Brandon Lew <brandon.t.lew@gmail.com>
2017-11-15 20:52:55 -0800

    minification and add currency data

--- js/controllers/tipCtrl.js
+++ js/controllers/tipCtrl.js
@@ -1,24 +1,51 @@
-app.controller('tipDataCtrl', function($scope, $http) {
-  $http.get('http://brandonsco.de/portfolio/tipjar/data/countryTipData.json').then(function(response) {
+app.controller('tipDataCtrl', ['$scope', '$http', function($scope, $http) {
+  // GET COUNTRY TIP DATA
+  $http.get('data/countryTipData-min.json').then(function(response) {
+    // WRITE COUNTRY LIST
     var countryName = [];
     for(var i = 0; i < response.data.length; i++) {
       countryName.push(response.data[i].country);
     }
     countryName.sort();
     $scope.countryTipData = countryName;
-    $scope.userSelection = function(tipData) {
+    // DISPLAY TIP DATA FOR COUNTRY SELECTION
+    $scope.userSelection = function(country) {
       document.getElementById('countryList').style.display = 'none';
       document.getElementById('countryTipData').style.display = 'block';
       for(var x = 0; x < response.data.length; x++) {
-        if(tipData === response.data[x].country) {
+        if(country === response.data[x].country) {
           $scope.country = response.data[x].country;
           $scope.dining = response.data[x].dining;
           $scope.transportation = response.data[x].transportation;
           $scope.accommodation = response.data[x].accommodation;
           $scope.thankyou = response.data[x].thankyou;
           $scope.goodbye = response.data[x].goodbye;
+          $scope.currency = response.data[x].currency;
+          // CURRENCY CONVERSION
+          var currency = response.data[x].currency;
+          $http.get('data/currencyData-min.json').then(function(response) {
+            for(var i = 0; i < response.data.length; i++) {
+              if(currency === response.data[i].currency) {
+                $scope.conversion = response.data[i].conversion;
+              }
+            }
+          })
+          // var srcCurrency = 'USD';
+          // var destCurrency = response.data[x].currency;
+          // var apiKey = 'c9912ecb5eb02559e15f08d6d3122f2c';
+          // var url = 'http://apilayer.net/api/live?access_key=' + apiKey + '&currencies=' + srcCurrency + ',' + destCurrency + '&format=1';
+          // $http.get(url).then(function(response) {
+          //   var array = [];
+          //   for(var key in response.data.quotes) {
+          //     if(response.data.quotes.hasOwnProperty(key)) {
+          //       array.push(key, response.data.quotes[key]);
+          //     }
+          //   }
+          //   $scope.conversion = array[3];
+          // })
+          // $scope.currency = response.data[x].currency;
         }
       }
     };
   });
-});
+}]);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// DISPLAY THE LIST OF COUNTRIES
 function showCountryList() {
   document.getElementById('banner').style.display = 'none';
   document.getElementById('countryList').style.display = 'block';
@@ -12,11 +13,13 @@ function showCountryList() {
 }
 document.getElementById('listBtn').addEventListener('click', showCountryList);
 
+// MAIN PAGE BANNER RANDOMIZER
 var photos = ["banner1.jpg","banner2.jpg","banner3.jpg","banner4.jpg","banner5.jpg"];
 var banner = document.getElementById('banner');
 var random = Math.floor(Math.random() * photos.length);
 banner.style.backgroundImage = 'url(img/' + photos[random] + ')';
 
+// HOVER EFFECT ON H3
 function mouseOverH3() {
   document.getElementById('h3').style.borderLeftColor = '#CC9F74';
   document.getElementById('h3').style.borderRightColor = '#CC9F74';
@@ -25,13 +28,14 @@ function mouseOverH3() {
 document.getElementById('listBtn').addEventListener('mouseover', mouseOverH3);
 function mouseOutH3() {
   document.getElementById('h3').style.borderLeftColor = 'initial';
-  document.getElementById('h3').style.borderRightColor = 'initial';  
+  document.getElementById('h3').style.borderRightColor = 'initial';
 }
 document.getElementById('listBtn').addEventListener('mouseout', mouseOutH3);
 
+// DISABLE HOVER EFFECT ON H3 FOR TOUCH DEVICES
 function mouseOverH3None() {
   document.getElementById('h3').style.borderLeftColor = 'initial';
-  document.getElementById('h3').style.borderRightColor = 'initial';  
+  document.getElementById('h3').style.borderRightColor = 'initial';
 }
 window.addEventListener('touchstart', function hover() {
   document.getElementById('listBtn').addEventListener('mouseover', mouseOverH3None);
